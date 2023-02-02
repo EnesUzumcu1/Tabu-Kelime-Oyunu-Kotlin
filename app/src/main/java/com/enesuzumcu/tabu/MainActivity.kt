93ef4f5d21c5cc19da5e461b52ab47c36fd574b8
Enes ÜZÜMCÜ <61823965+EnesUzumcu1@users.noreply.github.com>
2023-02-02 17:08:50 +0300

    Project created

---
+++ app/src/main/java/com/enesuzumcu/tabu/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.enesuzumcu.tabu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
